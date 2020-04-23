188839574f028c50141586ccc9b7f7991443462b
ilia-valchenko <i.valchenk@hotmail.com>
2020-04-23 22:00:16 +0300

    Implemented Delete page.

---
+++ OdeToFoodApplication/Pages/Restaurants/Delete.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Core.Entities;
+using OdeToFood.DataAccess.Repositories.Interfaces;
+
+namespace OdeToFoodApplication
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly IRestaurantRepository repository;
+
+        public Restaurant Restaurant { get; set; }
+
+        public DeleteModel(IRestaurantRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public IActionResult OnGet(int restaurantId)
+        {
+            Restaurant = repository.Get(restaurantId);
+
+            if (Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+
+            return Page();
+        }
+
+        public IActionResult OnPost(int restaurantId)
+        {
+            var restaurant = repository.Delete(restaurantId);
+
+            repository.Commit();
+
+            if (restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+
+            TempData["Message"] = $"{restaurant.Name} was successfully deleted.";
+
+            return RedirectToPage("./List");
+        }
+    }
+}
