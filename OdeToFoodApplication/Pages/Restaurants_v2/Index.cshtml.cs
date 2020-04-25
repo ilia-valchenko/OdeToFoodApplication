39e952dfcd9feebe86db34e7f8a17c4fc63b188a
ilia-valchenko <i.valchenk@hotmail.com>
2020-04-25 17:20:42 +0300

    Added scaffolding item.

---
+++ OdeToFoodApplication/Pages/Restaurants_v2/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using OdeToFood.Core.Entities;
+using OdeToFood.DataAccess;
+
+namespace OdeToFoodApplication
+{
+    public class IndexModel : PageModel
+    {
+        private readonly OdeToFood.DataAccess.OdeToFoodDbContext _context;
+
+        public IndexModel(OdeToFood.DataAccess.OdeToFoodDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Restaurant> Restaurant { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Restaurant = await _context.Restaurants.ToListAsync();
+        }
+    }
+}
