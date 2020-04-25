65bcf75e01d2a2e56288e22d8640ed022c9d0c5d
ilia-valchenko <i.valchenk@hotmail.com>
2020-04-25 12:09:09 +0300

    Implemented RestaurantCountViewComponent.

---
+++ OdeToFoodApplication/ViewComponents/RestaurantCountViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OdeToFood.DataAccess.Repositories.Interfaces;
+
+namespace OdeToFoodApplication.ViewComponents
+{
+    public sealed class RestaurantCountViewComponent : ViewComponent
+    {
+        private readonly IRestaurantRepository repository;
+
+        public RestaurantCountViewComponent(IRestaurantRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var count = repository.GetCount();
+            return View(count);
+        }
+    }
+}
